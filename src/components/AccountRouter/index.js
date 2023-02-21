157747fc7a03f85a2eb29aa270a448aa1cdf6771
prasanth <prasanth@example.com>
2023-02-21 07:39:44 +0000

    Account Router Updated

--- src/components/AccountRouter/index.js
+++ src/components/AccountRouter/index.js
@@ -1,3 +1,4 @@
+import {FaGoogle, FaTwitter, FaInstagram, FaYoutube} from 'react-icons/fa'
 import Header from '../Header'
 import './index.css'
 
@@ -6,7 +7,7 @@ const AccountRouter = () => (
     <Header boolValue="false" />
     <div className="account-container">
       <h1 className="account-heading">Account</h1>
-      <hr />
+      <hr className="horizontal-line" />
       <div className="memberShip-container">
         <p className="membership-heading">Member Ship</p>
         <div className="member-account-details">
@@ -16,17 +17,26 @@ const AccountRouter = () => (
           </p>
         </div>
       </div>
-      <hr />
+      <hr className="horizontal-line-2" />
       <div className="memberShip-container">
         <p className="membership-heading">Plan Details</p>
-        <div className="member-account-details">
-          <p className="member-userName">Premium</p>
+        <div className="member-account-password-container">
+          <p className="member-premium">Premium</p>
           <p className="quality-type">Ultra HD</p>
         </div>
       </div>
-      <hr />
-      <button type="button">Logout</button>
+      <hr className="horizontal-line-2" />
+      <button type="button" className="account-logout-button">
+        Logout
+      </button>
     </div>
+    <div className="contact-icons-container">
+      <FaGoogle className="google-icon" />
+      <FaTwitter className="google-icon" />
+      <FaInstagram className="google-icon" />
+      <FaYoutube className="google-icon" />
+    </div>
+    <p className="contact-heading">Contact</p>
   </div>
 )
 export default AccountRouter
