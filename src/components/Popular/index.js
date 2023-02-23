36bf3538fb040c340d43344a5a19b30c03184c9b
prasanth <prasanth@example.com>
2023-02-23 11:56:36 +0000

    Foot Section css changed

--- src/components/Popular/index.js
+++ src/components/Popular/index.js
@@ -1,13 +1,22 @@
 import {Component} from 'react'
 import {Link} from 'react-router-dom'
+import Loader from 'react-loader-spinner'
 import {FaGoogle, FaTwitter, FaInstagram, FaYoutube} from 'react-icons/fa'
 import Cookies from 'js-cookie'
 import Header from '../Header'
 import './index.css'
 
+const movieApiPopularStatus = {
+  initial: 'INITIAL',
+  success: 'SUCCESS',
+  failure: 'FAILURE',
+  inProgress: 'IN_PROGRESS',
+}
+
 class Popular extends Component {
   state = {
     popularMovies: [],
+    popularMovieStatus: movieApiPopularStatus.initial,
   }
 
   componentDidMount() {
@@ -15,6 +24,7 @@ class Popular extends Component {
   }
 
   getPopularMovies = async () => {
+    this.setState({popularMovieStatus: movieApiPopularStatus.inProgress})
     const url = 'https://apis.ccbp.in/movies-app/popular-movies'
     const jwtToken = Cookies.get('jwt_token')
     const options = {
@@ -33,16 +43,32 @@ class Popular extends Component {
         posterPath: eachMovie.poster_path,
         title: eachMovie.title,
       }))
-      this.setState({popularMovies: newList})
+      this.setState({
+        popularMovies: newList,
+        popularMovieStatus: movieApiPopularStatus.success,
+      })
+    } else {
+      this.setState({popularMovieStatus: movieApiPopularStatus.failure})
     }
   }
 
-  render() {
+  renderPopularLoader = () => (
+    <div className="popular-loader-container" testid="loader">
+      <Loader
+        type="TailSpin"
+        color="#D81F26"
+        height={50}
+        width={50}
+        className="popular-loader"
+      />
+    </div>
+  )
+
+  renderPopularSuccess = () => {
     const {popularMovies} = this.state
 
     return (
-      <div className="popular-container">
-        <Header boolValue="false" />
+      <>
         <ul className="popular-ul-item">
           {popularMovies.map(eachMovie => {
             const {id, backdropPath, title} = eachMovie
@@ -60,12 +86,60 @@ class Popular extends Component {
           })}
         </ul>
         <div className="contact-icons-container">
-          <FaGoogle className="popular-google-icon home-google" />
+          <FaGoogle className="popular-google-icon popular-google" />
           <FaTwitter className="popular-google-icon popular-twitter" />
           <FaInstagram className="popular-google-icon popular-instagram" />
           <FaYoutube className="popular-google-icon popular-facebook" />
         </div>
         <p className="popular-contact-heading">Contact</p>
+      </>
+    )
+  }
+
+  onRefreshPopularPage = () => {
+    this.getPopularMovies()
+  }
+
+  renderPopularFailure = () => (
+    <div className="popular-loader-container">
+      <img
+        src="https://res.cloudinary.com/duezhxznc/image/upload/v1677152293/Background-Complete_ojhbus.png"
+        alt="failure"
+        className="popular-failure-view"
+      />
+      <p className="popular-failure-view-name">
+        Something went wrong. Please try again
+      </p>
+      <button
+        type="button"
+        className="popular-failure-button"
+        onClick={this.onRefreshPopularPage}
+      >
+        Try Again
+      </button>
+    </div>
+  )
+
+  getPopularMovieList = () => {
+    const {popularMovieStatus} = this.state
+
+    switch (popularMovieStatus) {
+      case movieApiPopularStatus.inProgress:
+        return this.renderPopularLoader()
+      case movieApiPopularStatus.success:
+        return this.renderPopularSuccess()
+      case movieApiPopularStatus.failure:
+        return this.renderPopularFailure()
+      default:
+        return null
+    }
+  }
+
+  render() {
+    return (
+      <div className="popular-container">
+        <Header boolValue="false" />
+        {this.getPopularMovieList()}
       </div>
     )
   }
